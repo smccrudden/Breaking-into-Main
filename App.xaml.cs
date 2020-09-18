8cf62ca5e45da52b93d34209e9f48d98d1bc0e3c
smccrudden <70445252+smccrudden@users.noreply.github.com>
2020-09-18 13:19:38 -0500

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,45 @@
+﻿/* Name: Seth McCrudden
+ * Date: 9/16/2020
+ * File: App.xaml.cs.cpp
+ * Description: Creates a mainwindow, adds a textbox to it and then runs it
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Project
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        /*
+         * Description:
+         *      Creates a app, mainwindow and a textbox. Then adds the textbox to the mainwindow and runs it.
+         */
+        [STAThread]
+        public static void Main()
+        {
+            Project.App app = new Project.App();
+            app.InitializeComponent();
+            MainWindow mainWindow = new MainWindow();
+
+            TextBlock HelloFromMain = new TextBlock();
+            HelloFromMain.Text = "Hello from Main!";
+            HelloFromMain.FontSize = 18;
+            HelloFromMain.HorizontalAlignment = HorizontalAlignment.Center;
+            HelloFromMain.SetValue(DockPanel.DockProperty, Dock.Top);
+            mainWindow.HelloMainContainer.Children.Add(HelloFromMain);
+
+            app.Run(mainWindow);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿/* Name: Seth McCrudden
+ * Date: 9/16/2020
+ * File: MainWindow.xaml.cs
+ * Description: Creates an inkcanvas and handles all user interactions with
+ * it including: changing pen color, pen thickness and clearing the canvas
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /*
+         * Description:
+         *      The constructer sets the default values for pen thickness and pen color
+         */
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DrawingCanvas.DefaultDrawingAttributes.Width = DrawingCanvas.DefaultDrawingAttributes.Height = 1;
+            DrawingCanvas.DefaultDrawingAttributes.Color = Colors.Black;
+        }
+
+        /*
+         * Description:
+         *      An event handler which will be called then the user tries to change the pen size
+         */
+        private void SetPenSize(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (DrawingCanvas != null)
+            {
+                DrawingCanvas.DefaultDrawingAttributes.Width = penSize.Value;
+                DrawingCanvas.DefaultDrawingAttributes.Height = penSize.Value;
+            }
+        }
+
+        /*
+         * Description:
+         *      An event handler which will be called then the user tries to change the pen color
+         */
+        private void ChangePenColor(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (DrawingCanvas != null)
+            {
+                Color selectedColor = Color.FromRgb((byte)penColorR.Value, (byte)penColorG.Value, (byte)penColorB.Value);
+                DrawingCanvas.DefaultDrawingAttributes.Color = selectedColor;
+                ColorPreview.Background = new SolidColorBrush(selectedColor);
+            }
+        }
+
+        /*
+         * Description:
+         *      An event handler which will be called then the user tries to clear the canvas
+         */
+        private void ResetCanvas(object sender, RoutedEventArgs e)
+        {
+            DrawingCanvas.Strokes.Clear();
+        }
+
+        /*
+         * Description:
+         *      An event handler which will be called then the user tries to reset the pen size
+         *      back to the default. The function modifies the slider value for pensize and lets
+         *      the event handler handle the actual thickness change
+         */
+        private void ResetThickness(object sender, RoutedEventArgs e)
+        {
+            penSize.Value = 1;
+        }
+
+        /*
+         * Description:
+         *      An event handler which will be called then the user tries to reset the pen color
+         *      back to the default. The function modifies the slider value for penColorR, penColorG,
+         *      penColorB and lets the event handler handle the actual color change
+         */
+        private void ResetColors(object sender, RoutedEventArgs e)
+        {
+            penColorR.Value = penColorG.Value = penColorB.Value = 0;
+        }
+    }
+}
